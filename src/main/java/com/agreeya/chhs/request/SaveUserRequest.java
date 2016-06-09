c760feaf36ac8261289b772a54f643aef8836f45
Amit-sharma-Lead <amit.sharma@agreeya.net>
2016-06-09 18:22:38 +0530

    updated

--- src/main/java/com/agreeya/chhs/request/SaveUserRequest.java
+++ src/main/java/com/agreeya/chhs/request/SaveUserRequest.java
@@ -167,7 +167,7 @@ public class SaveUserRequest extends AbstractRequiresUserContext implements WSRe
 		}
 
 		if (fam.getNumberOfKids() == null || fam.getNumberOfKids().isEmpty()) {
-			vr.addMessage("Kids is not provided!.");
+			fam.setNumberOfKids("0");
 		}
 
 		if (fam.getKidsPref() == null || fam.getKidsPref().isEmpty()) {
